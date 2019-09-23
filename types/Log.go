6632e72b139e79871d8a34459e38ed916642dfd7
Mona Lisa <nikruzhok@gmail.com>
2019-09-24 00:52:55 +0300

    second commit

---
+++ types/Log.go
@@ -0,0 +1,12 @@
+package types
+
+import "time"
+
+type Log struct {
+	Timestamp time.Time `json:timestamp`
+	Host      string    `json:host`
+	Project   string    `json:project`
+	Logname   string    `json:logname`
+	Level     string    `json:level`
+	Message   string    `json:message`
+}
