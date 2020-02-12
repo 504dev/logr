4f1f6f6774c48218dbf4d7e3fd12f9063f8c84d0
Mona Lisa <dkozhurkin@gmail.com>
2020-02-12 18:11:57 +0300

    recieve ws messages

--- controllers/ws.go
+++ controllers/ws.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"github.com/504dev/kidlog/config"
+	"github.com/504dev/kidlog/logger"
 	"github.com/504dev/kidlog/types"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -13,8 +14,35 @@ import (
 type WsController struct{}
 
 func (wc WsController) Index(c *gin.Context) {
-	handler := websocket.Handler(wc.Reader)
+	handler := websocket.Handler(wc.Reader2)
 	handler.ServeHTTP(c.Writer, c.Request)
+}
+
+type message struct {
+	// the json tag means this will serialize as a lowercased field
+	Message string `json:"message"`
+}
+
+func (_ WsController) Reader2(ws *websocket.Conn) {
+	for {
+		// allocate our container struct
+		var m message
+
+		// receive a message using the codec
+		if err := websocket.JSON.Receive(ws, &m); err != nil {
+			logger.Error(err)
+			break
+		}
+
+		logger.Debug("Received message:", m.Message)
+
+		// send a response
+		m2 := message{"Thanks for the message!"}
+		if err := websocket.JSON.Send(ws, m2); err != nil {
+			logger.Error(err)
+			break
+		}
+	}
 }
 
 func (_ WsController) Reader(ws *websocket.Conn) {
