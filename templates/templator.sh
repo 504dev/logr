2f445415bdce397a81290fe0cfded60a8d077cfa
Habib the Sweet <13257902+habib-the-sweet@users.noreply.github.com>
2020-08-27 16:22:57 +0300

    add macos and linux sed workaround

--- templates/templator.sh
+++ templates/templator.sh
@@ -9,9 +9,16 @@ if [ ! -f ".env" ]; then
     exit 1
 fi
 
-if ! sha256sum --version &> /dev/null; then
-    function sha256sum() { shasum -a 256 "$@" ; } && export -f sha256sum
-fi
+if ! sha256sum --version &> /dev/null; then                                                             
+    function sha256sum() { shasum -a 256 "$@" ; } && export -f sha256sum                                                                                                        
+fi 
+
+# Default case for Linux sed, just use "-i"
+SEDI=(-i)
+case "$(uname)" in
+  # For macOS, use two parameters
+  Darwin*) SEDI=(-i "")
+esac
 
 source .env
 
@@ -21,32 +28,30 @@ CLICKHOUSE_PASSWORD_HASH=$(echo -n "${CLICKHOUSE_PASSWORD}" | sha256sum | awk '{
 
 cp templates/user.xml.template "${CLICKHOUSE_USER_FILE}"
 
-sed -i'' -e "s/\${CLICKHOUSE_USER\}/${CLICKHOUSE_USER}/g" "${CLICKHOUSE_USER_FILE}"
-sed -i'' -e "s/\${CLICKHOUSE_PASSWORD_HASH\}/${CLICKHOUSE_PASSWORD_HASH}/g" "${CLICKHOUSE_USER_FILE}"
+sed "${SEDI[@]}" -e "s/CLICKHOUSE_USER/${CLICKHOUSE_USER}/g" "${CLICKHOUSE_USER_FILE}"
+sed "${SEDI[@]}" -e "s/CLICKHOUSE_PASSWORD_HASH/${CLICKHOUSE_PASSWORD_HASH}/g" "${CLICKHOUSE_USER_FILE}"
 
 ## Create Logr config from template
 LOGR_CONFIG_FILE="config.yml"
 
 cp templates/config.yml.template "${LOGR_CONFIG_FILE}"
 
-sed -i'' -e "s/\${LOGR_HTTP_BIND\}/${LOGR_HTTP_BIND}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${LOGR_UDP_BIND\}/${LOGR_UDP_BIND}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/LOGR_HTTP_BIND/${LOGR_HTTP_BIND}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/LOGR_UDP_BIND/${LOGR_UDP_BIND}/g" "${LOGR_CONFIG_FILE}"
 
-sed -i'' -e "s/\${MYSQL_HOST\}/${MYSQL_HOST}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${MYSQL_PORT\}/${MYSQL_PORT}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${MYSQL_ROOT_PASSWORD\}/${MYSQL_ROOT_PASSWORD}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${MYSQL_DATABASE\}/${MYSQL_DATABASE}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${MYSQL_USER\}/${MYSQL_USER}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${MYSQL_PASSWORD\}/${MYSQL_PASSWORD}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/MYSQL_HOST/${MYSQL_HOST}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/MYSQL_PORT/${MYSQL_PORT}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/MYSQL_ROOT_PASSWORD/${MYSQL_ROOT_PASSWORD}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/MYSQL_DATABASE/${MYSQL_DATABASE}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/MYSQL_USER/${MYSQL_USER}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" "${LOGR_CONFIG_FILE}"
 
-sed -i'' -e "s/\${CLICKHOUSE_HOST\}/${CLICKHOUSE_HOST}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${CLICKHOUSE_PORT\}/${CLICKHOUSE_PORT}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${CLICKHOUSE_DATABASE\}/${CLICKHOUSE_DATABASE}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${CLICKHOUSE_USER\}/${CLICKHOUSE_USER}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${CLICKHOUSE_PASSWORD\}/${CLICKHOUSE_PASSWORD}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/CLICKHOUSE_HOST/${CLICKHOUSE_HOST}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/CLICKHOUSE_PORT/${CLICKHOUSE_PORT}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/CLICKHOUSE_DATABASE/${CLICKHOUSE_DATABASE}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/CLICKHOUSE_USER/${CLICKHOUSE_USER}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/CLICKHOUSE_PASSWORD/${CLICKHOUSE_PASSWORD}/g" "${LOGR_CONFIG_FILE}"
 
-sed -i'' -e "s/\${OAUTH_JWT_SECRET\}/${OAUTH_JWT_SECRET}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${OAUTH_GITHUB_ORG\}/${OAUTH_GITHUB_ORG}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${OAUTH_GITHUB_CLIENT_ID\}/${OAUTH_GITHUB_CLIENT_ID}/g" "${LOGR_CONFIG_FILE}"
-sed -i'' -e "s/\${OAUTH_GITHUB_CLIENT_SECRET\}/${OAUTH_GITHUB_CLIENT_SECRET}/g" "${LOGR_CONFIG_FILE}"
-
+sed "${SEDI[@]}" -e "s/OAUTH_JWT_SECRET/${OAUTH_JWT_SECRET}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/OAUTH_GITHUB_CLIENT_ID/${OAUTH_GITHUB_CLIENT_ID}/g" "${LOGR_CONFIG_FILE}"
+sed "${SEDI[@]}" -e "s/OAUTH_GITHUB_CLIENT_SECRET/${OAUTH_GITHUB_CLIENT_SECRET}/g" "${LOGR_CONFIG_FILE}"
