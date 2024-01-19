1534bf42da480afd405ee27a221478a15100fc1d
kozhurkin <dkozhurkin@gmail.com>
2024-01-19 22:48:27 +0300

    .AddDate(0, 0, -1).Format(time.DateOnly)

--- models/log/log.go
+++ models/log/log.go
@@ -22,7 +22,7 @@ func GetByFilter(f types.Filter) (types.Logs, error) {
 		if f.Offset != 0 {
 			now = time.Unix(0, f.Offset)
 		}
-		day := now.UTC().AddDate(0, 0, -1).Format(time.DateOnly)[0:10]
+		day := now.UTC().AddDate(0, 0, -1).Format(time.RFC3339)[0:10]
 		w1 := fmt.Sprintf("%v AND day = '%v'", where, day)
 		logs, err := getByFilter(w1, values, limit)
 		if err != nil {
