e75e2cfce8f7618394902fda2bd2aaca07642aea
kozhurkin <dkozhurkin@gmail.com>
2024-01-20 17:46:51 +0300

    cipher cleanup

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -1,13 +1,11 @@
 package cipher
 
 import (
-	"crypto"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
-	"crypto/sha512"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
@@ -44,33 +42,6 @@ func PrivateKeyToBytes(priv *rsa.PrivateKey) []byte {
 
 func PublicKeyToBytes(pub *rsa.PublicKey) ([]byte, error) {
 	return x509.MarshalPKIXPublicKey(pub)
-}
-
-func BytesToPrivateKey(priv []byte) (*rsa.PrivateKey, error) {
-	return x509.ParsePKCS1PrivateKey(priv)
-}
-
-func BytesToPublicKey(pub []byte) (*rsa.PublicKey, error) {
-	ifc, err := x509.ParsePKIXPublicKey(pub)
-	if err != nil {
-		return nil, err
-	}
-	key, ok := ifc.(*rsa.PublicKey)
-	if !ok {
-		err := errors.New("Not ok")
-		return nil, err
-	}
-	return key, nil
-}
-
-func EncryptWithPublicKey(msg []byte, pub *rsa.PublicKey) ([]byte, error) {
-	hash := sha512.New()
-	return rsa.EncryptOAEP(hash, rand.Reader, pub, msg, nil)
-}
-
-func DecryptWithPrivateKey(ciphertext []byte, priv *rsa.PrivateKey) ([]byte, error) {
-	hash := sha512.New()
-	return rsa.DecryptOAEP(hash, rand.Reader, priv, ciphertext, nil)
 }
 
 func EncryptAesJson(data interface{}, priv string) (string, error) {
@@ -144,14 +115,4 @@ func DecryptAes(cipherText []byte, key []byte) ([]byte, error) {
 	stream.XORKeyStream(cipherText, cipherText)
 
 	return cipherText, nil
-}
-
-func SignMessage(msg []byte, priv *rsa.PrivateKey) ([]byte, error) {
-	digest := sha256.Sum256(msg)
-	return rsa.SignPKCS1v15(rand.Reader, priv, crypto.SHA256, digest[:])
-}
-
-func CheckSig(msg []byte, sig []byte, pub *rsa.PublicKey) error {
-	digest := sha256.Sum256(msg)
-	return rsa.VerifyPKCS1v15(pub, crypto.SHA256, digest[:], sig)
 }
