27ca1b26a56cf29bbde2994d541e8188459ceb10
kozhurkin <dkozhurkin@gmail.com>
2024-03-12 03:40:03 +0300

    golang v19

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-git pull origin master --tags --recurse-submodules
-docker-compose up --build -d
+git pull origin master --tags --recurse-submodules &&
+docker-compose up --build -d &&
 docker system prune -a -f
