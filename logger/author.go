3626a5e4ecc34b8723e08b1ea9412bb9d68346b1
kozhurkin <dkozhurkin@gmail.com>
2024-04-27 20:19:54 +0300

    delay split \n

--- logger/author.go
+++ logger/author.go
@@ -117,10 +117,10 @@ func author(conf *lgc.Config) {
 			return
 		}
 		history = append(history, body.Answer())
-		chunks := strings.Split(body.Answer().Content, "\n")
+		chunks := strings.Split(body.Answer().Content, ". ")
 		for _, chunk := range chunks {
-			log.Info(chunk)
-			<-time.After(time.Second)
+			log.Info("%s. ", chunk)
+			<-time.After(time.Second * 3)
 		}
 	}
 }
