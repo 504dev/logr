287b0ce0f364dd3b5eba961360dcfacd6aaffe96
kozhurkin <dkozhurkin@gmail.com>
2024-04-29 00:22:46 +0300

    ai.log

--- logger/ai.go
+++ logger/ai.go
@@ -29,6 +29,74 @@ type ResponseBody struct {
 	Done    bool            `json:"done"`
 }
 
+func ai(conf *lgc.Config) {
+	defer func() {
+		<-time.After(10 * time.Second)
+		ai(conf)
+	}()
+
+	log, _ := conf.NewLogger("ai.log")
+	log.Body = "[{version}] {message}"
+
+	n := 5
+	genres := []string{
+		"Romance",
+		"Biographies & Memoirs",
+		"Literary Fiction",
+		"Science Fiction",
+		"Drama",
+		"Horror",
+		"Mysticism",
+		"Phantasmagoria",
+		"Detective",
+		"Novel",
+		"Fantasy",
+		"Adventure",
+		"Comedy",
+		"Manga and anime",
+		"Young Adult",
+		"Self-Help",
+	}
+	genre := genres[time.Now().Nanosecond()%len(genres)]
+	prompt := fmt.Sprintf(`Imagine that you are a writer in the %s genre.
+Think of the title of a book about a monitoring service called "Logr", which was developed by a 30-year-old developer from Saint-Petersburg named Dima.
+Then state the genre of the book.
+Then make a table of contents of 5 chapters.
+Then write a 100-word summary of the book.`, genre, n)
+	history := ChatHistory{
+		{Role: "user", Content: prompt},
+	}
+
+	onToken := func(t string) { log.Inc("tokens", 1) }
+
+	answer, err := Prompt(history, func(s string) { log.Notice(s) }, onToken)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	history = append(history, answer)
+
+	log.Info("")
+	for i := 1; i <= n; i++ {
+		prompt := fmt.Sprintf(`Write chapter %v, as long as you can.`, i)
+		if i == n {
+			prompt += "This is the last chapter, ending the book epically."
+		}
+		history = append(history, &ChatHistoryItem{Role: "user", Content: prompt})
+
+		answer, err := Prompt(history, func(s string) { log.Info(s) }, onToken)
+
+		if err != nil {
+			log.Error(err)
+			return
+		}
+
+		history = append(history, answer)
+		log.Info("")
+	}
+}
+
 func Prompt(history ChatHistory, onSentence func(string), onToken func(string)) (*ChatHistoryItem, error) {
 	OLLAMA_MODEL := config.Get().DemoDash.Model
 	OLLAMA_CHAT_URL := config.Get().DemoDash.Url
@@ -70,65 +138,6 @@ func Prompt(history ChatHistory, onSentence func(string), onToken func(string)) 
 	onSentence(tmp)
 
 	return &answer, nil
-}
-
-func ai(conf *lgc.Config) {
-	defer func() {
-		<-time.After(10 * time.Second)
-		ai(conf)
-	}()
-
-	log, _ := conf.NewLogger("ai.log")
-	log.Body = "[{version}] {message}"
-
-	n := 5
-	genres := []string{
-		"science fiction",
-		"drama",
-		"horror",
-		"mysticism",
-		"phantasmagoria",
-		"detective",
-		"novel",
-		"fantasy",
-		"adventure",
-		"comedy",
-	}
-	genre := genres[time.Now().Nanosecond()%len(genres)]
-	prompt := fmt.Sprintf(`Imagine that you are a writer in the %s genre.
-Think of the title of a book about a monitoring service called logr, which was developed by a 30-year-old developer from St. Petersburg named Dima.
-Then state the genre of the book.
-Then make a table of contents of %s short chapter titles.
-Then write a 100-word summary of the book.`, genre, n)
-	history := ChatHistory{
-		{Role: "user", Content: prompt},
-	}
-
-	onToken := func(t string) { log.Inc("tokens", 1) }
-
-	answer, err := Prompt(history, func(s string) { log.Notice(s) }, onToken)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
-	history = append(history, answer)
-
-	log.Info("")
-	for i := 1; i <= n; i++ {
-		prompt := fmt.Sprintf(`Write chapter %v, as long as you can.`, i)
-		history = append(history, &ChatHistoryItem{Role: "user", Content: prompt})
-
-		answer, err := Prompt(history, func(s string) { log.Info(s) }, onToken)
-
-		if err != nil {
-			log.Error(err)
-			return
-		}
-
-		history = append(history, answer)
-		log.Info("")
-	}
 }
 
 func splitIntoSentences(text string) []string {
