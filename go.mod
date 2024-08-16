e5a4c01a94ecafd119fbe45a201d45fc67c43808
kozhurkin <dkozhurkin@gmail.com>
2024-08-16 19:44:02 +0300

    tests/LogPackageJoiner_test.go

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/504dev/logr
 go 1.19
 
 require (
-	github.com/504dev/logr-go-client v1.60.10
+	github.com/504dev/logr-go-client v1.61.0
 	github.com/ClickHouse/clickhouse-go v1.5.4
 	github.com/dustin/go-humanize v1.0.1
 	github.com/fatih/color v1.16.0
@@ -18,20 +18,22 @@ require (
 	github.com/orcaman/concurrent-map/v2 v2.0.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pressly/goose/v3 v3.9.0
-	golang.org/x/net v0.23.0
-	golang.org/x/oauth2 v0.16.0
-	golang.org/x/sync v0.6.0
+	github.com/stretchr/testify v1.8.4
+	golang.org/x/net v0.28.0
+	golang.org/x/oauth2 v0.20.0
+	golang.org/x/sync v0.8.0
 	google.golang.org/grpc v1.62.1
-	google.golang.org/protobuf v1.33.0
+	google.golang.org/protobuf v1.34.2
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/bytedance/sonic v1.11.2 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/cloudflare/golz4 v0.0.0-20150217214814-ef862a3cdc58 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -40,7 +42,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
@@ -52,6 +54,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/shirou/gopsutil/v3 v3.24.2 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
@@ -61,10 +64,9 @@ require (
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/arch v0.7.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ tests/LogPackageJoiner_test.go
@@ -0,0 +1,95 @@
+package tests
+
+import (
+	_types "github.com/504dev/logr-go-client/types"
+	"github.com/504dev/logr/types"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Предположим, что _types.LogPackage и _types.LogPackageChunks определены в отдельном пакете
+// Для целей тестирования мы создадим упрощенные версии этих структур
+
+func TestLogPackageJoiner_Add(t *testing.T) {
+	joiner := types.NewLogPackageJoiner(time.Millisecond*10, 3)
+
+	// Тест добавления первого чанка
+	lp1 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test1", I: 0, N: 3}, PlainLog: []byte("chunk1")}
+	complete, joined := joiner.Add(lp1)
+	assert.False(t, complete)
+	assert.Nil(t, joined)
+
+	// Тест добавления второго чанка
+	lp2 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test1", I: 1, N: 3}, PlainLog: []byte("chunk2")}
+	complete, joined = joiner.Add(lp2)
+	assert.False(t, complete)
+	assert.Nil(t, joined)
+
+	// Тест добавления последнего чанка и завершения пакета
+	lp3 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test1", I: 2, N: 3}, PlainLog: []byte("chunk3")}
+	complete, joined = joiner.Add(lp3)
+	assert.True(t, complete)
+	assert.NotNil(t, joined)
+	assert.Equal(t, []byte("chunk1chunk2chunk3"), joined.PlainLog)
+}
+
+func TestLogPackageJoiner_AddOutOfOrder(t *testing.T) {
+	joiner := types.NewLogPackageJoiner(time.Millisecond*10, 3)
+
+	lp1 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test2", I: 0, N: 3}, PlainLog: []byte("chunk1")}
+	lp2 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test2", I: 1, N: 3}, PlainLog: []byte("chunk2")}
+	lp3 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test2", I: 2, N: 3}, PlainLog: []byte("chunk3")}
+	chunks := _types.LogPackageChunks{lp2, lp1, lp3}
+
+	var wg sync.WaitGroup
+	var joined *_types.LogPackage
+
+	wg.Add(len(chunks))
+	for i, lp := range chunks {
+		go func(i int, lp *_types.LogPackage) {
+			defer wg.Done()
+			<-time.After(time.Millisecond * time.Duration(i+1))
+			complete, j := joiner.Add(lp)
+			if complete {
+				joined = j
+				assert.NotNil(t, joined)
+			} else {
+				assert.Nil(t, joined)
+			}
+		}(i, lp)
+	}
+
+	wg.Wait()
+
+	assert.Equal(t, []byte("chunk1chunk2chunk3"), joined.PlainLog)
+}
+
+func TestLogPackageJoiner_AddTimeout(t *testing.T) {
+	joiner := types.NewLogPackageJoiner(time.Millisecond*10, 2)
+
+	lp1 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test3", I: 0, N: 2}, PlainLog: []byte("chunk1")}
+	joiner.Add(lp1)
+
+	time.Sleep(time.Millisecond * 30) // Ждем, пока пакет устареет
+
+	lp2 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test3", I: 1, N: 2}, PlainLog: []byte("chunk2")}
+	complete, joined := joiner.Add(lp2)
+	assert.False(t, complete)
+	assert.Nil(t, joined)
+}
+
+func TestLogPackageJoiner_AddCiphered(t *testing.T) {
+	joiner := types.NewLogPackageJoiner(time.Millisecond*10, 3)
+
+	lp1 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test4", I: 0, N: 2}, CipherLog: []byte("encrypted1")}
+	joiner.Add(lp1)
+
+	lp2 := &_types.LogPackage{Chunk: &_types.ChunkInfo{Uid: "test4", I: 1, N: 2}, CipherLog: []byte("encrypted2")}
+	complete, joined := joiner.Add(lp2)
+	assert.True(t, complete)
+	assert.NotNil(t, joined)
+	assert.Equal(t, []byte("encrypted1encrypted2"), joined.CipherLog)
+}
